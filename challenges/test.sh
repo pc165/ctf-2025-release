16f97eb59c370b96c1516cdad93b4b979c1b83dc
chenp59 <pablo.chen@roche.com>
2025-10-22 21:08:27 +0200

    add test

---
+++ challenges/test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+curl -fs -o /dev/null https://cardx.ctf.hackthelab.online
+curl -fs -o /dev/null https://dating.ctf.hackthelab.online
+curl -fs -o /dev/null https://hangman-one.ctf.hackthelab.online
+curl -fs -o /dev/null https://hangman-two.ctf.hackthelab.online
+curl -fs -o /dev/null https://hangman-three.ctf.hackthelab.online
+curl -fs -o /dev/null https://last.ctf.hackthelab.online
+curl -fs -o /dev/null https://moa-station.ctf.hackthelab.online
+curl -fs -o /dev/null https://pathing.ctf.hackthelab.online
+curl -fs -o /dev/null https://sighting.ctf.hackthelab.online
+curl -fs -o /dev/null https://slackblock-steg.ctf.hackthelab.online
+curl -fs -o /dev/null https://web-tutorial-1.ctf.hackthelab.online
+curl -fs -o /dev/null https://web-tutorial-2.ctf.hackthelab.online
+curl -fs -o /dev/null https://block-cipher.ctf.hackthelab.online
+curl -fs -o /dev/null https://hidden-reports.ctf.hackthelab.online
+curl -fs -o /dev/null https://hoard.ctf.hackthelab.online
+curl -fs -o /dev/null https://taxonomy.ctf.hackthelab.online
+curl -fs -o /dev/null https://detector.ctf.hackthelab.online
+curl -fs -o /dev/null https://detector-2.ctf.hackthelab.online
+curl -fs -o /dev/null https://evidence.ctf.hackthelab.online
+curl -fs -o /dev/null https://extinction.ctf.hackthelab.online
+curl -fs -o /dev/null https://go-git-it.ctf.hackthelab.online
+
+nc -z arrow-of-time.ctf.hackthelab.online 9000
+nc -z cert-tool.ctf.hackthelab.online 9001
+nc -z digital-citizen.ctf.hackthelab.online 9002
+nc -z flag-search.ctf.hackthelab.online 9003
+nc -z goto-zero.ctf.hackthelab.online 9004
+nc -z pascals-homomorphism.ctf.hackthelab.online 9005
+nc -z amd.ctf.hackthelab.online 9006
+nc -z bug-me.ctf.hackthelab.online 9007
+nc -z drago-daction.ctf.hackthelab.online 9008
+nc -z go-back.ctf.hackthelab.online 9009
+nc -z meow.ctf.hackthelab.online 9010
